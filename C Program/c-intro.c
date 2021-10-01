90b022ac7674f872a52acc0fab4b5ca3627bf429
bisheskafle <goodkafle@gmail.com>
2021-10-01 11:20:33 +0545

    C Introduction Program

---
+++ C Program/c-intro.c
@@ -0,0 +1,21 @@
+#include<conio.h>
+
+#include<stdio.h>
+
+void main()
+
+{
+
+clrscr( );
+
+char name[20];
+
+printf(“Enter you name:”);
+
+scanf(“%s”, name);
+
+printf(“\n Hello %s”,name);
+
+getch( );
+
+}
