73d14e4a81be13456bd01831d145109fbbab7de8
Prateek Labroo <50594243+PLabroo@users.noreply.github.com>
2021-10-01 13:22:19 +0530

    New Solution
    I just refactored the code and gave a new solution to it.Propsoing these new changes for hactoberfest 2021
    Thanks

--- C++ Program/Array/Remove Duplicates from Sorted Array/solution.cpp
+++ C++ Program/Array/Remove Duplicates from Sorted Array/solution.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
+    int resultremoveDuplicate(vector<int>&arr,int n)
+    {
+        if (n==0 || n==1)
+        return n;
+        int j = 0;
+       
+        for (int i=0; i < n-1; i++)
+        if (arr[i] != arr[i+1])
+            arr[j++] = arr[i];
+ 
+       arr[j++] = arr[n-1];
+ 
+       return j;
+    }
     int removeDuplicates(vector<int>& a) {
-        int temp=-101;
-        int count=0;
-        for(int i=0;i<a.size();i++){
-            if(a[i]!=temp){
-                temp=a[i];
-                count++;
-            }
-            else{
-                a[i]=101;
-            }
-        }
-        sort(a.begin(),a.end());
-        return count;
+       int result = resultremoveDuplicate(a,a.size());
+        return result;
     }
 };
