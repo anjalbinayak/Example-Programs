5aed8bef5de418f8bff863f7028266482a83db4d
Sujan Rai <sznroe@gmail.com>
2021-10-04 21:37:55 +0545

    Add JS program to guess a number from user

---
+++ Js Program/guessNumber.js
@@ -0,0 +1,16 @@
+function guessNumber() {
+
+    const random = Math.floor(Math.random() * 10) + 1;
+
+    let number = parseInt(prompt('Guess a number from 1 to 10: '));
+
+    while (number !== random) {
+        number = parseInt(prompt('Guess a number from 1 to 10: '));
+    }
+
+    if (number == random) {
+        console.log('You guessed the correct number.');
+    }
+
+}
+guessNumber();
