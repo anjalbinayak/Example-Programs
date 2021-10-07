4789c3f31e55de45c2d78a03d3d4abefe5b50df5
Srishti Gauraha <67550382+Srishti44-g@users.noreply.github.com>
2021-10-07 21:32:17 +0530

    Add files via upload

---
+++ Java Program/Blockdemo.java
@@ -0,0 +1,18 @@
+/*  Demonstrate a block of code.
+     Call this  file BlockDemo.java
+*/
+class BlockDemo{
+   public static void main(String args[])
+ {
+  double i,j,d;
+  i = 5;
+  j  = 10;
+    //the target of this is  in code
+     if(i!=0)
+   {
+      System.out.println("i does not equal zero");
+      d = j/i;
+      System.out.println(" j/i  is  "+d);
+   }													 
+ }
+}
